23eed1f5c4f56e57c24d7ad549e6dfd3203d8148
turnerandy17 <turnerandy17@gmail.com>
2021-06-10 22:09:30 -0700

    First pass at line graph, scatterplot

--- Graphics.R
+++ Graphics.R
@@ -3,4 +3,44 @@
 library(tidyverse)
 library(coefplot)
 library(corrplot)
+library(readr)
 
+
+spy <- read_csv("01. Raw Data/SPY.csv")
+View(spy)
+
+
+# Line Graph (for time series data) ----
+ggplot(spy, mapping = aes(spy$Date, spy$`Adj Close`)) +
+  geom_line(color = "cadetblue4") +
+  labs(x = "Date",
+       y = "Adj. Close Price",
+       title = "SPDR S&P 500 ETF Trust (SPY)",
+       subtitle = "Historical Data",
+       caption = "Yahoo Finance") +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(color = ),
+        plot.title = element_text(hjust = 0.5, size = 20, face = "bold",
+                                  color = "dodgerblue4"),
+        plot.subtitle = element_text(hjust = 0.5, size = 14, color = "royalblue4"),
+        plot.caption = element_text(color = "gray60"))
+
+# (Scatterplot w/line) ----
+ggplot(spy, aes(spy$Date, spy$Volume)) +
+  geom_point(aes(y = spy$Volume), color = "cadetblue4") +
+  geom_hline(yintercept = 0, col = "slategrey", linetype = "solid") +
+  labs(x = "Date",
+       y = "Volume",
+       title = "SPDR S&P 500 ETF Weekly Volume",
+       subtitle = "Historical Data",
+       caption = "Yahoo Finance") +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(color = ),
+        plot.title = element_text(hjust = 0.5, size = 20, face = "bold",
+                                  color = "dodgerblue4"),
+        plot.subtitle = element_text(hjust = 0.5, size = 10, color = "slategrey"),
+        plot.caption = element_text(color = "gray60"))
